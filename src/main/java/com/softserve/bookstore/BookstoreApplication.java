941fc7b636a27b8a48c42224e142ce05052c18d9
mlasc <lascumihai31@gmail.com>
2023-04-06 09:02:28 +0300

    small changes to book repo

--- src/main/java/com/softserve/bookstore/BookstoreApplication.java
+++ src/main/java/com/softserve/bookstore/BookstoreApplication.java
@@ -2,15 +2,10 @@ package com.softserve.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
 @SpringBootApplication
 public class BookstoreApplication {
 
-	public static void main(String[] args) throws SQLException, IOException {
+	public static void main(String[] args){
 
 		 SpringApplication.run(BookstoreApplication.class, args);
 	}
